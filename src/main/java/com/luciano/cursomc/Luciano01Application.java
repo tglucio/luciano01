6ddbbd8df8faed498154863e5d92f713ce5547e9
Lucianosmvm <luciiano_ariiane@hotmail.com>
2020-02-05 22:43:47 -0300

    criado projeto

---
+++ src/main/java/com/luciano/cursomc/Luciano01Application.java
@@ -0,0 +1,13 @@
+package com.luciano.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Luciano01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Luciano01Application.class, args);
+	}
+
+}
